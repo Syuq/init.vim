7dcf17d14a76247561ff436d9ecf2647deefc805
Syuq <tatd.wine@gmail.com>
2024-06-13 16:04:54 +0700

    📦 deps(go.nvim): include guihua.lua, nvim-lspconfig, and nvim-treesitter

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -51,5 +51,19 @@ local plugins = {
         vim.cmd [[silent! GoInstallDeps]]
       end,
     },
+    {
+      "ray-x/go.nvim",
+      dependencies = {  -- optional packages
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      config = function()
+        require("go").setup()
+      end,
+      event = {"CmdlineEnter"},
+      ft = {"go", 'gomod'},
+      build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
   }
   return plugins
